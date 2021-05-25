e9482365800435d3a1ea43d802f92343892de72e
nurdoganyavuz <nurdogan.yavuz@ogrenci.karatay.edu.tr>
2021-05-25 17:55:53 +0300

    25.05.2021 - Last update

--- ITS/wwwroot/pageScripts/Requests/Edit.js
+++ ITS/wwwroot/pageScripts/Requests/Edit.js
@@ -1,1 +1,10 @@
-﻿
+﻿$(document).click(function () {
+    $('#RequestStatus').change(function () {
+        if ($('#RequestStatus').val() == 2) {
+            $('#redStatus').show();
+        }
+        else {
+            $('#redStatus').hide();
+        }
+    });
+});
